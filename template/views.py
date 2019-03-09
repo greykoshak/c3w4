deea9a35cc57b5ce47bad47a86b388de021c901c
gk <greykoshak@gmail.com>
2019-03-09 13:40:38 +0200

    Templates: 3/3 done + source v1.0

--- template/views.py
+++ template/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-from django.utils.http import urlencode
+
+
+# from django.utils.http import urlencode
 
 
 def echo(request):
+    get_letter, get_value = list(), list()
+
+    if request.method == 'GET':
+        get_letter = [key for key in request.GET]
+        get_value = [request.GET[key] for key in request.GET]
+    elif request.method == 'POST':
+        get_letter = [key for key in request.POST]
+        get_value = [request.POST[key] for key in request.POST]
+
     return render(request, 'echo.html', context={
-        'get_letter': request.META['QUERY_STRING'][0],
-        'get_value': request.GET.get(request.META['QUERY_STRING'][0]),
+        'get_letter': get_letter[0] if len(get_letter) > 0 else "",
+        'get_value': get_value[0] if len(get_value) > 0 else "",
         'get_tag': request.META.get('HTTP_X_PRINT_STATEMENT'),
-        'request_method': request.META['REQUEST_METHOD'].lower()
+        'request_method': request.method.lower() if len(get_letter) > 0 else "",
+        'colon': ':' if len(get_letter) > 0 else ""
     }, status=200)
 
 
