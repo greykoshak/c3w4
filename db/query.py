a83a5b2f0f36be84c6c225ba1b647dd7abcc28f1
gk <greykoshak@gmail.com>
2019-03-09 21:03:05 +0200

    django orm: query first

--- db/query.py
+++ db/query.py
@@ -1,13 +1,29 @@
-from datetime import datetime
-
-from django.db.models import Q, Count, Avg
-from pytz import UTC
-
 from db.models import User, Blog, Topic
 
 
 def create():
-    pass
+    u1 = User.objects.create(first_name='u1', last_name='u1')
+    u1.save()
+    u2 = User.objects.create(first_name='u2', last_name='u2')
+    u2.save()
+    u3 = User.objects.create(first_name='u3', last_name='u3')
+    u3.save()
+
+    blog1 = Blog.objects.create(title='blog1', author='u1')
+    blog1.save()
+    blog2 = Blog.objects.create(title='blog2', author='u1')
+    blog2.save()
+
+    blog1.subscribers.add(u1, u2)
+    blog2.subscribers.add(u2)
+    blog1.save()
+    blog2.save()
+
+    topic1 = Topic.objects.create(title='topic2_content', blog=blog1, author=u3, created='2017-01-01')
+    topic1.save()
+
+    topic1.likes.add(u1, u2, u3)
+    topic1.save()
 
 
 def edit_all():
@@ -34,7 +50,7 @@ def get_topic_title_ended():
     pass
 
 
-def get_user_with_limit():
+def get_u_with_limit():
     pass
 
 
@@ -54,11 +70,11 @@ def get_topic_by_u1():
     pass
 
 
-def get_user_that_dont_have_blog():
+def get_u_that_dont_have_blog():
     pass
 
 
-def get_topic_that_like_all_users():
+def get_topic_that_like_all_us():
     pass
 
 
